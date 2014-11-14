6f89c53d6a1e84d3c3662109bdb4177374b7391f
Will Oram <will@infosnap.com>
2014-11-14 11:02:23 -0600

    Adjudicator stub

---
+++ judge.js
@@ -0,0 +1,23 @@
+var DiplomacyJudge = module.exports = function(options) {
+
+};
+
+DiplomacyJudge.prototype = {
+    /**
+     * Resolves one phase of a game.
+     * @param  {Object} phase
+     * @return {Object} A new phase containing resolved positions.
+     */
+    resolve: function(phase) {
+
+    },
+
+    /**
+     * Resolves a chronologically ordered array of phases.
+     * @param  {Array} phases
+     * @return {Object} A new phase containing resolved positions.
+     */
+    resolveAll: function(phases) {
+
+    }
+};
