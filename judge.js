9452397fd6068f6a132804f25c6f8bb41f18e274
Will Oram <will@infosnap.com>
2014-12-09 14:00:15 -0600

    Added basic middleware tests

--- judge.js
+++ judge.js
@@ -1,8 +1,15 @@
-var DiplomacyJudge = module.exports = function(options) {
-
+var DiplomacyJudge = module.exports = function(variant, options) {
+    if(!(this instanceof DiplomacyJudge)) return new DiplomacyJudge(variant, options);
+    if (!variant)
+        throw new Error('No variant data supplied.');
+        
+    var _variant = variant;
+        
+    options = options || { };
 };
 
 DiplomacyJudge.prototype = {
+    
     /**
      * Resolves one phase of a game.
      * @param  {Array} phase
@@ -10,6 +17,8 @@ DiplomacyJudge.prototype = {
      */
     resolve: function(phase) {
         // A phase is an array of PlayerSeason objects with one object per player.
+        if (!phase)
+            throw new Error('No phase data supplied.');
          
         /*
          * Minimum requirements to verify before doing anything:
@@ -25,21 +34,21 @@ DiplomacyJudge.prototype = {
             if (!year)
                 year = order.year;
             else if (year !== order.year)
-                throw 'The year ' + year + ' is not consistent in this phase\'s orders.';
+                throw new Error('The year ' + year + ' is not consistent in this phase\'s orders.');
                 
             if (!season)
                 season = order.season;
             else if (season !== order.season)
-                throw 'The season is not consistent in ' + year + '\'s orders.';
+                throw new Error('The season is not consistent in ' + year + '\'s orders.');
                 
             if (powers.indexOf(order.power) > -1)
-                throw order.power + ' has multiple orders in ' + year + '\'s phase.';
+                throw new Error(order.power + ' has multiple orders in ' + year + '\'s phase.');
             else
                 powers.push(order.power);
         }
         
         // Begin resolution.
-        for var p = 0; p < phase.length; p++) {
+        for (var p = 0; p < phase.length; p++) {
         
         }
     },
@@ -47,7 +56,7 @@ DiplomacyJudge.prototype = {
     /**
      * Resolves a chronologically ordered array of phases.
      * @param  {Array} phases
-     * @return {Object} A new phase containing resolved positions.
+     * @return {Array} A new phase containing resolved positions.
      */
     resolveAll: function(phases) {
 
---
+++ test/init.spec.js
@@ -0,0 +1,33 @@
+var request = require('supertest'),
+    express = require('express');
+
+var app = express();
+var judge = require('../judge.js');
+
+describe('Middleware init scenarios', function() {
+    it('requires variant data', function() {
+        expect(function() { new judge(); }).toThrow(new Error('No variant data supplied.'));
+    });
+    
+    it('does not require option data', function() {
+        // Jasmine's toThrow() is weird: http://stackoverflow.com/a/13233194/260460
+        expect(function() { new judge({ }) }).not.toThrow();
+    });
+});
+
+describe('Invalid data scenarios', function() {
+    var r = new judge({ }).resolve;
+    
+    it('requires phase data', function() {
+        expect(function() { r(); }).toThrow(new Error('No phase data supplied.'));
+    });
+    
+    it('expects all moves\' years to match', function() {
+        var goodData = [{ year: 1901, season: 1, power: 'A' }, { year: 1901, season: 1, power: 'B' }],
+            badData  = [{ year: 1901, season: 1, power: 'A' }, { year: 1902, season: 1, power: 'B' }];
+            
+        expect(function() {
+            r(badData);
+        }).toThrow(new Error('The year 1901 is not consistent in this phase\'s orders.'));
+    });
+});
