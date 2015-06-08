e797ef70468134dd071065838cfd4594d5e37f2b
spamguy <spamguy@spamguy.org>
2015-06-08 11:25:45 -0500

    Further building out of DATC spec file

--- test/datc.spec.js
+++ test/datc.spec.js
@@ -5,9 +5,24 @@ var fs = require('fs'),
     stream = fs.createReadStream('test/datc_v2.4_06.txt', { encoding: 'utf8' }),
     stream = byline.createStream(stream),
     expect = require('expect.js'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    Judge = require('../judge');
 
-var variant = null;
+var variant = null,
+    judge = null;
+
+/*
+ * During a unit test parse, a line can be considered in one of several states.
+ *
+ * Line parsing will stay in that state's section until the next state change.
+ */
+var UnitTestSubstateType = {
+    TEST: 0,
+    PRESTATE: 1,
+    ORDERS: 2,
+    POSTSTATE: 3
+},
+    currentSubstate = UnitTestSubstateType.TEST;
 
 var clearCommentReg = new RegExp(/^\s*#.*$/),
     variantReg = new RegExp(/^VARIANT_ALL\s+(\S*)\s*$/),
@@ -17,10 +32,25 @@ var clearCommentReg = new RegExp(/^\s*#.*$/),
     ordersReg = new RegExp(/^([^:]+):\s+(.*)$/),
     preOrderReg = new RegExp(/^(SUCCESS|FAILURE):\s+([^:]+):\s+(.*)$/);
 
+// substates within unit tests
+var inPrestateSetPhaseMode = false;
+
 var itQueue = [ ],              // queue up it()s to be run later
-    itLabel = 1,
-    itData = null,
-    itExpectations = [ ];
+    itLabel = '',
+    currentPhase,
+    expectedPhase,
+    genericIt = function(l, judge, before, after) {
+        var actualAfter = judge.process(before);
+
+        it(l, function() { expect(1).to.equal(1); });
+    };
+
+// wraps enqueued it() tests with correct params
+var itWrapper = function(fn, context, params) {
+        return function() {
+            fn.apply(context, params);
+        };
+    };
 
 stream.on('error', function(err) {
     console.log(err);
@@ -34,17 +64,43 @@ stream.on('data', function(line) {
         // use match in the context of variant file names
         match = _.camelCase(match[1]);
         variant = JSON.parse(fs.readFileSync('../../variants/' + match + '/' + match + '.json', { encoding: 'utf8' }));
+
+        // instantiate judge
+        judge = new Judge(variant);
+    }
+    else if (match = line.match(caseReg)) {
+        itLabel = match[1];
+
+        // start new old/expected phases to build
+        beforePhaseData = afterPhaseData = { moves: [ ] };
+    }
+    else if (match = line.match(prestateSetPhaseReg)) {
+        // enter prestate processing mode
+        currentSubstate = UnitTestSubstateType.PRESTATE;
     }
     else if (line.trim() === 'END') {
         // test has been built and can be run after the file has been processed
-        itQueue.push(function() { it(itLabel++, function() { expect(1).to.equal(1); }); });
+        itQueue.push(itWrapper(genericIt, this, [itLabel, judge, beforePhaseData, afterPhaseData]));
+    }
+    else {
+        // if none of the above apply, we must be in a substate of some sort
+        switch (currentSubstate) {
+            case UnitTestSubstateType.PRESTATE:
+            break;
+
+            case UnitTestSubstateType.ORDERS:
+            break;
+
+            case UnitTestSubstateType.POSTSTATE:
+            break;
+        }
     }
 });
 
 stream.on('end', function() {
     // run all tests
     describe('DATC', function() {
-        for (var i = 0; i < itQueue.length; i++)
-            itQueue[i]();
+        while (itQueue.length > 0)
+            (itQueue.shift())();
     });
 });
