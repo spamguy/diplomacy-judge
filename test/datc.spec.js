ccf82763e927b6cf4b6a3a744f7fbdc2b5eed331
spamguy <spamguy@spamguy.org>
2015-06-22 11:32:04 -0500

    'TRAVIS_BUILD_DIR' not defined? Whatever

--- test/datc.spec.js
+++ test/datc.spec.js
@@ -89,7 +89,7 @@ stream.on('data', function(line) {
 
             var variantPath;
             if (process.env.TRAVIS)
-                variantPath = path.resolve(path.join(TRAVIS_BUILD_DIR, 'variants/' + match + '/' + match + '.json'));
+                variantPath = path.resolve(path.join('/home/travis/build/spamguy/diplomacy-judge', 'variants/' + match + '/' + match + '.json'));
             else
                 variantPath = path.resolve(path.join(__dirname, '../../../variants/' + match + '/' + match + '.json'));
 
